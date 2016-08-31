071b4899e95b6abab61684f33296d78034aa25b5
amirf2020 <arminghafouri8869.8869@gmail.com>
2016-08-31 03:28:31 -0700

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,16 +1,16 @@
 do
 
 local function run(msg, matches)
-      local text = "Beyond Self Bot V1.0\nAn Fun Bot Based On TeleSeed Written In Lua\n\nSudo User :\nDeveloper&Founder : @SoLiD021\nDeVeloper&Manager : @CliApi\n\nTeam Channel :\n@BeyondTeam\n\nSpecial Thx To :\nSeed Team\nAnd All My Friends :D\n\nBeyond Self Bot Version 1.0 On GitHub :\nGithub.com/BeyondTeam/Self-Bot"
-  if matches[1]:lower() == 'beyondself' or 'version' or 'ver' or 'git' then --change this with anything you want
+      local text = "alfro Self Bot V1.0\nAn Fun Bot Based On TeleSeed Written In Lua\n\nSudo User :\nDeveloper&Founder : @SoLiD021\nDeVeloper&Manager : @CliApi\n\nTeam Channel :\n@cli_api\n\nSpecial Thx To :\nSeed Team\nAnd All My Friends :D\n\nBeyond Self Bot Version 1.0 On GitHub :\nGithub.com/amirf2020/Self-Bot-1"
+  if matches[1]:lower() == 'alfroself' or 'version' or 'ver' or 'git' then --change this with anything you want
 reply_msg(msg.id, text, ok_cb, false)
   end
 end
 
 return {
   patterns = {
-    "^[!/#]([Bb]eyondself)$",
-    "^([Bb]eyondself)$",
+    "^[!/#]([Aa]lfself)$",
+    "^([Aa]lfroself)$",
     "^[!/#]([Vv]ersion)$",
     "^([Vv]ersion)$",
     "^([Gg]it)$",
